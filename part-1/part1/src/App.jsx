f1251c81e3a9e69578fc2745a5d9bd1bc76f1b79
Emilia Orlinova <emilia.orlinova@gmail.com>
2024-09-29 15:42:31 +0300

    exercise 1.6

--- part-1/part1/src/App.jsx
+++ part-1/part1/src/App.jsx
@@ -1,59 +1,24 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} - {props.exersice}
-    </p>
-  );
-};
+import { useState } from "react";
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.parts[0].name} exersice={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exersice={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exersice={props.parts[2].exercises} />
-    </div>
-  );
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises:
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-};
-
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h2>Give feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 };
