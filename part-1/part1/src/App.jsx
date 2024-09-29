f76205044c60905e9724b37079ec51ef4ab8b13f
Emilia Orlinova <emilia.orlinova@gmail.com>
2024-09-29 16:02:22 +0300

    exersice 1.8

--- part-1/part1/src/App.jsx
+++ part-1/part1/src/App.jsx
@@ -3,13 +3,27 @@ import { useState } from "react";
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
+
+const Statistics = ({ good, neutral, bad, all }) => {
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {good && bad ? (good - bad) / all : 0}</p>
+      <p>positive {good ? (good / all) * 100 : 0} %</p>
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
-  console.log(good);
   return (
     <div>
       <h2>Give feedback</h2>
@@ -34,13 +48,7 @@ const App = () => {
         }}
         text="bad"
       />
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {good && bad ? (good - bad) / all : 0}</p>
-      <p>positive {good ? (good / all) * 100 : 0} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   );
 };
