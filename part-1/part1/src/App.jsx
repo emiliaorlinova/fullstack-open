3debd41e1d93af92c48b50f25a72420b33cc8769
Emilia Orlinova <emilia.orlinova@gmail.com>
2024-09-29 15:55:12 +0300

    exersice 1.7

--- part-1/part1/src/App.jsx
+++ part-1/part1/src/App.jsx
@@ -8,17 +8,39 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  const [all, setAll] = useState(0);
+  console.log(good);
   return (
     <div>
       <h2>Give feedback</h2>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Button
+        onClick={() => {
+          setGood(good + 1);
+          setAll(all + 1);
+        }}
+        text="good"
+      />
+      <Button
+        onClick={() => {
+          setNeutral(neutral + 1);
+          setAll(all + 1);
+        }}
+        text="neutral"
+      />
+      <Button
+        onClick={() => {
+          setBad(bad + 1);
+          setAll(all + 1);
+        }}
+        text="bad"
+      />
       <h2>Statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {good && bad ? (good - bad) / all : 0}</p>
+      <p>positive {good ? (good / all) * 100 : 0} %</p>
     </div>
   );
 };
