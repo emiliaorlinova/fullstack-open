0ca1c9802429789739f90368bcab627e69f13b51
Emilia Orlinova <emilia.orlinova@gmail.com>
2024-09-30 16:19:50 +0300

    exersice 1.12

--- part-1/part1/src/App.jsx
+++ part-1/part1/src/App.jsx
@@ -1,75 +1,27 @@
 import { useState } from "react";
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tbody>
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    </tbody>
-  );
-};
-
-const Statistics = ({ good, neutral, bad, all }) => {
-  return (
-    <>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine
-        text="average"
-        value={good && bad ? (good - bad) / all : 0}
-      />
-      <StatisticLine text="positive" value={good ? (good / all) * 100 : 0} />
-    </>
-  );
-};
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  const randomNumber = () => Math.floor(Math.random() * 8);
+
+  console.log(selected);
 
   return (
     <div>
-      <h2>Give feedback</h2>
-      <Button
-        onClick={() => {
-          setGood(good + 1);
-          setAll(all + 1);
-        }}
-        text="good"
-      />
-      <Button
-        onClick={() => {
-          setNeutral(neutral + 1);
-          setAll(all + 1);
-        }}
-        text="neutral"
-      />
-      <Button
-        onClick={() => {
-          setBad(bad + 1);
-          setAll(all + 1);
-        }}
-        text="bad"
-      />
-      <h2>Statistics</h2>
-      {good || neutral || bad ? (
-        <table>
-          <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-        </table>
-      ) : (
-        <p>No feedback given</p>
-      )}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={() => setSelected(randomNumber)}>next anecdote</button>
     </div>
   );
 };
